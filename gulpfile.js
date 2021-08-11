9cd51c9c74b8651a045b90de824a72ae0088594a
Matt Graham <m.j.graham@live.co.uk>
2021-08-11 21:59:10 +0100

    getting shot of unused css

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const del = require('del');
+const purgecss = require('gulp-purgecss');
 
 gulp.task('styles', () => {
 	return bulma = gulp.src('src/main/resources/static/scss/style.scss').
 		pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('src/main/resources/static/css/'));
+		.pipe(gulp.dest('src/main/resources/static/css/raw'));
 });
 
 gulp.task('clean', () => {
 	return del([
 		'src/main/resources/static/css/style.css',
+		'src/main/resources/static/css/raw/style.css'
 	]);
-})
+});
 
-gulp.task('default', gulp.series(['clean', 'styles']));
+gulp.task('purgecss', () => {
+	return gulp.src('src/main/resources/static/css/raw/style.css')
+		.pipe(purgecss({
+			content: ['src/main/resources/templates/*.html', 'src/main/resources/templates/**/*.html']
+		}))
+		.pipe(gulp.dest('src/main/resources/static/css'))
+});
+
+gulp.task('default', gulp.series(['clean', 'styles', 'purgecss']));
